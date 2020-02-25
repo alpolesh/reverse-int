693f5c5eb4fe78d38f57a7305b948a983968e83e
Andrei Paleshchuk <alpoleshuck@gmail.com>
2020-02-26 01:39:35 +0300

    feat: solve reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse (n) {
-  
+ function reverse (n) {
+    let str = String(n);
+    let arr = str.split('').reverse();
+        if (arr[arr.length-1] == '-'){
+            arr.pop();
+        }
+    
+    
+    return +arr.join('');
 }
+console.log(reverse(-120));
+
+module.exports = reverse;
